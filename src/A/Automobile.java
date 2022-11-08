aa1f448ccbbac6ee1ae20c5c81414942eeff2f21
me <me>
2022-11-08 09:37:41 +0100

    Aggiunta di classe Automobile che implementa Veicolo

---
+++ src/A/Automobile.java
@@ -0,0 +1,13 @@
+package A;
+
+public class Automobile extends Veicolo {
+    @Override
+    public String nome() {
+        return "Automobile";
+    }
+
+    @Override
+    public String cilindrata() {
+        return "Tanta";
+    }
+}
